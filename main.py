19beafb813d475cde4bf7c8803a8d81b707a17b1
Joe Patrick <joe.boo.patrick@gmail.com>
2025-04-11 15:32:14 +0100

    added sorted letter list

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from stats import get_word_count
+import sys
+from stats import get_word_count, get_letter_count, get_sorted_dict
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    book_text = (get_book_text("books/frankenstein.txt"))
+    filepath = "books/frankenstein.txt"
+    book_text = (get_book_text(filepath))
     word_count = get_word_count(book_text)
-    print(f"{word_count} words found in the document")
+    letter_count = get_letter_count(book_text)
+    letter_sort = (get_sorted_dict(letter_count))
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for i in letter_sort:
+        if i["letter"].isalpha():
+            print(f"{i["letter"]}: {i["count"]}")
+
+    
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,3 +1,21 @@
 def get_word_count(text):
     num_words = text.split()
     return len(num_words)
+
+def get_letter_count(text):
+    letter_count = {}
+    text_lower = text.lower()
+    for i in text_lower:
+        if i in letter_count:
+            letter_count[i] += 1
+        else:
+            letter_count[i] = 1
+    return letter_count
+
+def get_sorted_dict(dict):
+    letter_list = []
+    for letter, count in dict.items():
+        letter_list.append({"letter": letter, "count": count})
+   # return letter_list
+    return sorted(letter_list, key=lambda x: x["count"], reverse=True)
+    
